4dd2dde2ad30d65f784f5a813a729dc533277b7b
LaurenceRichard <ls.richard@wanadoo.fr>
2019-11-08 14:54:30 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoList';
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCBanlox3w06RPLym_95eaW2KybJ8shti4",
+      authDomain: "todolist-73bfd.firebaseapp.com",
+      databaseURL: "https://todolist-73bfd.firebaseio.com",
+      projectId: "todolist-73bfd",
+      storageBucket: "todolist-73bfd.appspot.com",
+      messagingSenderId: "493726617287",
+      appId: "1:493726617287:web:73cebb860e5575b35578e2"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
+
 }
